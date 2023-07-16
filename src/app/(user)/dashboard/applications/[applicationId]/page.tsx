891b137a1b5bdcbba91e06491441e10a2c9d6710
Romain Pierucci <pierucci.romain@gmail.com>
2023-07-16 19:12:58 +0200

    feat: add application analytics page

---
+++ src/app/(user)/dashboard/applications/[applicationId]/page.tsx
@@ -0,0 +1,12 @@
+type ApplicationDashboard = {};
+
+const ApplicationDashboard = () => {
+  return (
+    <>
+      <h1>Application dashboard</h1>
+      <p>{"Ici les composants KPIS analytics de l'application sélectionnée"}</p>
+    </>
+  );
+};
+
+export default ApplicationDashboard;
