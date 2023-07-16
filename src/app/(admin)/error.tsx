e715e708c54f9a3af375c3d6589775eba950b712
Milan <pro@turtletv.fr>
2023-07-16 22:47:14 +0200

    ✨ Karl pas content

--- src/app/(admin)/error.tsx
+++ src/app/(admin)/error.tsx
@@ -6,6 +6,8 @@ import spin from "@assets/spin.json";
 import { Button, Spacer } from "@nextui-org/react";
 import Lottie from "lottie-react";
 import { useRouter } from "next/navigation";
+import Confetti from "react-confetti";
+import useWindowSize from "react-use/lib/useWindowSize";
 
 export default function Error({
   error,
@@ -14,6 +16,7 @@ export default function Error({
   error: Error;
   reset: () => void;
 }) {
+  const { width, height } = useWindowSize();
   const [user, setUser] = useLocalStorage<User>("user", initialUser);
 
   const router = useRouter();
@@ -28,6 +31,19 @@ export default function Error({
         width: "35rem",
       }}
     >
+      <Confetti
+        width={width}
+        height={height}
+        numberOfPieces={50}
+        wind={0}
+        recycle={true}
+        drawShape={(ctx) => {
+          // draw image
+          const img = new Image();
+          img.src = "https://i.imgur.com/r6VZngj.jpg";
+          ctx.drawImage(img, 0, 0, 100, 100);
+        }}
+      />
       <Lottie animationData={spin} />
       <h4>{error.message}</h4>
       <Spacer y={2} />
