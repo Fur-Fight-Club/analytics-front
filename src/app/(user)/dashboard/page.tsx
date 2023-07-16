acf619468fbf7a985dafb755ee492ad69b8d00a1
Milan <pro@turtletv.fr>
2023-07-16 13:49:25 +0200

    ✨ Add credentials

--- src/app/(user)/dashboard/page.tsx
+++ src/app/(user)/dashboard/page.tsx
@@ -2,10 +2,13 @@
 
 import { useLocalStorage } from "@/hooks/localStorage.hook";
 import { initialUser, User } from "@/types/user.type";
-import { Card, Grid, Spacer, Text } from "@nextui-org/react";
+import { Card, Grid, Input, Spacer, Text } from "@nextui-org/react";
+import crypto from "crypto";
 
 export default function DashboardPage() {
   const [user, setUser] = useLocalStorage<User>("user", initialUser);
+  var token = crypto.randomBytes(48).toString("hex");
+  console.log(token);
 
   return (
     <div>
@@ -19,7 +22,7 @@ export default function DashboardPage() {
       <Spacer y={1} />
       {user.isVerified === true ? (
         <div>
-          <Grid.Container>
+          <Grid.Container gap={2}>
             <Grid xs={12} md={4}>
               <Card>
                 <Card.Header>
@@ -53,7 +56,38 @@ export default function DashboardPage() {
                 </Card.Body>
               </Card>
             </Grid>
-            <Grid xs={12} md={4}></Grid>
+            <Grid xs={12} md={4}>
+              <Card>
+                <Card.Header>
+                  <Text h3>Mes identifiants</Text>
+                </Card.Header>
+                <Card.Body>
+                  {user.clientId && user.clientSecret ? (
+                    <>
+                      <Input
+                        label="Client ID"
+                        value={user.clientId}
+                        fullWidth
+                        contentEditable={false}
+                      />
+                      <Spacer y={0.5} />
+                      <Input
+                        label="Client Secret"
+                        value={user.clientSecret}
+                        fullWidth
+                        contentEditable={false}
+                      />
+                    </>
+                  ) : (
+                    <Text>
+                      Votre compte a été validé, mais vous n'avez pas encore
+                      d'identifiants pour vous connecter à l'API. Veuillez
+                      revenir ici plus tard
+                    </Text>
+                  )}
+                </Card.Body>
+              </Card>
+            </Grid>
             <Grid xs={12} md={4}></Grid>
           </Grid.Container>
         </div>
