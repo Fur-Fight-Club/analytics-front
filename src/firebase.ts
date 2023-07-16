f89502b192fdd8d80632c8dfb9dccffbdc5b76f1
Milan <pro@turtletv.fr>
2023-07-16 12:39:09 +0200

    🗃️ Add user operations

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,13 @@
 import firebase, { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
+import { User, getAuth } from "firebase/auth";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { UserDb } from "./types/user.type";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCZ1viKnJtYQfHEYAUHZTLCdnulMu-bfsI",
@@ -14,11 +21,40 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
+const user = {
+  get: async (uid: string): Promise<UserDb> => {
+    const userDoc = await db.doc(db.firestore, "users", uid);
+    const userDocData = await db.getDoc(userDoc);
+    const userData = userDocData.data();
+    return {
+      isVerified: userData!.isVerified,
+      companyName: userData!.companyName,
+      kbis: userData!.kbis,
+      phoneNumber: userData!.phoneNumber,
+      websiteURL: userData!.websiteURL,
+      role: userData!.role,
+      clientId: userData!.clientId,
+      clientSecret: userData!.clientSecret,
+    };
+  },
+  update: async (uid: string, data: Partial<UserDb>) => {
+    try {
+      const docRef = db.doc(db.firestore, "users", uid);
+      await updateDoc(docRef, data);
+      console.log("Document updated successfully!");
+    } catch (error) {
+      console.error("Error updating document:", error);
+    }
+  },
+};
+
 export const auth = getAuth(app);
 export const db = {
   firestore: getFirestore(app),
   doc,
   setDoc,
   getDoc,
+  updateDoc,
+  user,
 };
 export default app;
